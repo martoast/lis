ab07a730a95cfb2f08910f2d64fd61d85cb10c70
dev <alexmartos96@gmail.com>
2020-05-13 14:17:49 -0700

    getting price data into chart

--- components/sections/ProductList.vue
+++ components/sections/ProductList.vue
@@ -1,30 +1,46 @@
 <template>
-  <div v-if="flag === true">
-    <v-data-table
-      :headers="headers"
-      :items="desserts"
-      :items-per-page="5"
-      class="elevation-1"
-    ></v-data-table>
-  </div>
+  <v-card>
+    <v-card-title>
+      <v-spacer></v-spacer>
+      <v-text-field
+        v-model="search"
+        append-icon="mdi-magnify"
+        label="Search"
+        single-line
+        hide-details
+      ></v-text-field>
+    </v-card-title>
+    <div v-if="flag === true">
+      <v-data-table
+        :headers="headers"
+        :items="products"
+        :items-per-page="5"
+        :search="search"
+        class="elevation-1"
+      ></v-data-table>
+    </div>
+  </v-card>
 </template>
 <script>
 export default {
   data() {
     return {
+      products: [],
+      search: "",
       flag: false,
       headers: [
         {
-          text: "Dessert (100g serving)",
+          text: "name",
           align: "start",
           sortable: false,
           value: "name"
         },
-        { text: "Price", value: "calories" },
-        { text: "Amount", value: "fat" },
-        { text: "Carbs (g)", value: "carbs" },
-        { text: "Protein (g)", value: "protein" },
-        { text: "Iron (%)", value: "iron" }
+        // { text: "price", value: "calories" },
+        { text: "quantity", value: "quantity" }
+        // { text: "gram (g)", value: "gram" },
+        // { text: "eight (1/8)", value: "eight" },
+        // { text: "quarter (1/4)", value: "quarter" },
+        // { text: "oz (24g)", value: "oz" }
       ],
       desserts: [
         {
@@ -128,6 +144,23 @@ export default {
                 querySnapshot.forEach(function(doc) {
                   // doc.data() is never undefined for query doc snapshots
                   console.log(doc.id, " => ", doc.data());
+                  var PriceList = doc.data().price;
+                  for (var key in PriceList) {
+                    console.log(key, PriceList[key]);
+
+                    let headerthing = { text: key, value: key };
+
+                    let productsthing = { text: key, value: PriceList[key] };
+
+                    vm.headers.push(headerthing);
+
+                    // vm.products.push(productsthing);
+
+                    // let thing = {text: PriceList[i], value: }
+                  }
+
+                  vm.products.push(doc.data());
+                  console.log(vm.products);
                 });
               })
               .catch(function(error) {
