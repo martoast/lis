024613ae163d044874c169e6942950936588d059
dev <alexmartos96@gmail.com>
2020-05-13 19:15:41 -0700

    product data showing in table

--- components/sections/ProductList.vue
+++ components/sections/ProductList.vue
@@ -35,94 +35,12 @@ export default {
           sortable: false,
           value: "name"
         },
-        // { text: "price", value: "calories" },
-        { text: "quantity", value: "quantity" }
-        // { text: "gram (g)", value: "gram" },
-        // { text: "eight (1/8)", value: "eight" },
-        // { text: "quarter (1/4)", value: "quarter" },
-        // { text: "oz (24g)", value: "oz" }
-      ],
-      desserts: [
-        {
-          name: "Frozen Yogurt",
-          calories: 159,
-          fat: 6.0,
-          carbs: 24,
-          protein: 4.0,
-          iron: "1%"
-        },
-        {
-          name: "Ice cream sandwich",
-          calories: 237,
-          fat: 9.0,
-          carbs: 37,
-          protein: 4.3,
-          iron: "1%"
-        },
-        {
-          name: "Eclair",
-          calories: 262,
-          fat: 16.0,
-          carbs: 23,
-          protein: 6.0,
-          iron: "7%"
-        },
-        {
-          name: "Cupcake",
-          calories: 305,
-          fat: 3.7,
-          carbs: 67,
-          protein: 4.3,
-          iron: "8%"
-        },
-        {
-          name: "Gingerbread",
-          calories: 356,
-          fat: 16.0,
-          carbs: 49,
-          protein: 3.9,
-          iron: "16%"
-        },
-        {
-          name: "Jelly bean",
-          calories: 375,
-          fat: 0.0,
-          carbs: 94,
-          protein: 0.0,
-          iron: "0%"
-        },
-        {
-          name: "Lollipop",
-          calories: 392,
-          fat: 0.2,
-          carbs: 98,
-          protein: 0,
-          iron: "2%"
-        },
-        {
-          name: "Honeycomb",
-          calories: 408,
-          fat: 3.2,
-          carbs: 87,
-          protein: 6.5,
-          iron: "45%"
-        },
-        {
-          name: "Donut",
-          calories: 452,
-          fat: 25.0,
-          carbs: 51,
-          protein: 4.9,
-          iron: "22%"
-        },
-        {
-          name: "KitKat",
-          calories: 518,
-          fat: 26.0,
-          carbs: 65,
-          protein: 7,
-          iron: "6%"
-        }
+
+        { text: "quantity", value: "quantity" },
+        { text: "gram (g)", value: "gram" },
+        { text: "eight (1/8)", value: "eight" },
+        { text: "quarter (1/4)", value: "quarter" },
+        { text: "oz (24g)", value: "oz" }
       ]
     };
   },
@@ -144,20 +62,21 @@ export default {
                 querySnapshot.forEach(function(doc) {
                   // doc.data() is never undefined for query doc snapshots
                   console.log(doc.id, " => ", doc.data());
+
                   var PriceList = doc.data().price;
-                  for (var key in PriceList) {
-                    console.log(key, PriceList[key]);
+                  // for (var key in PriceList) {
+                  //   console.log(key, PriceList[key]);
 
-                    let headerthing = { text: key, value: key };
+                  //   let headerthing = { text: key, value: key };
 
-                    let productsthing = { text: key, value: PriceList[key] };
+                  //   let productsthing = { text: key, value: PriceList[key] };
 
-                    vm.headers.push(headerthing);
+                  //   vm.headers.push(headerthing);
 
-                    // vm.products.push(productsthing);
+                  // vm.products.push(productsthing);
 
-                    // let thing = {text: PriceList[i], value: }
-                  }
+                  // let thing = {text: PriceList[i], value: }
+                  // }
 
                   vm.products.push(doc.data());
                   console.log(vm.products);
