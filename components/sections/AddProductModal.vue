37225fffc8387e8794b00291e83052d9eaf7eddc
dev <alexmartos96@gmail.com>
2020-05-14 00:21:43 -0700

    parsed so firebases stores numbers for prices

--- components/sections/AddProductModal.vue
+++ components/sections/AddProductModal.vue
@@ -1,12 +1,14 @@
 <template>
-  <v-row justify="center">
+  <v-row>
     <v-dialog v-model="dialog" persistent max-width="390">
       <template v-slot:activator="{ on }">
-        <v-btn color="primary" dark v-on="on">Add Product</v-btn>
+        <v-btn color="primary" dark v-on="on" rounded
+          ><v-icon>mdi-plus-circle-outline</v-icon></v-btn
+        >
       </template>
       <v-card>
         <v-card-title>
-          <span class="headline">User Profile</span>
+          <span class="headline">Add Product</span>
         </v-card-title>
         <v-card-text>
           <v-container>
@@ -22,7 +24,6 @@
                 <v-autocomplete
                   :items="['Sativa', 'Hybrid', 'Indica']"
                   label="Type"
-                  multiple
                   v-model="type"
                 ></v-autocomplete>
               </v-col>
@@ -121,7 +122,7 @@ export default {
       quarter: "",
       oz: "",
       image: "",
-      quantity: "",
+      quantity: null,
       available: []
     };
   },
@@ -133,13 +134,13 @@ export default {
           name: this.name,
           type: this.type,
           details: this.details,
-          gram: this.gram,
-          eight: this.eight,
-          quarter: this.quarter,
-          oz: this.oz,
+          gram: parseInt(this.gram),
+          eight: parseInt(this.eight),
+          quarter: parseInt(this.quarter),
+          oz: parseInt(this.oz),
           image: this.image,
           available: this.available,
-          quantity: this.quantity
+          quantity: parseInt(this.quantity)
         });
       } catch (e) {
         alert(e);
@@ -147,6 +148,16 @@ export default {
       }
       alert("Success.");
       this.dialog = false;
+      this.name = "";
+      this.type = "";
+      this.details = "";
+      this.gram = "";
+      this.eight = "";
+      this.quarter = "";
+      this.oz = "";
+      this.image = "";
+      this.quantity = null;
+      this.available = [];
     }
   }
 };
